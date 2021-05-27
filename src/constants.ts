93bfc9a5f4537ec93b01024a31555f7ea0b47a07
Sniped <facto@facto.club>
2021-05-26 22:33:23 -0400

    comment constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
-export const REPO_NAME = 'plugin-test';
+// repo constants
+// TODO: allow management of the artifacts of multiple repos
 
+export const REPO_NAME = 'plugin-test';
 export const REPO_OWNER = 'Sniped';
